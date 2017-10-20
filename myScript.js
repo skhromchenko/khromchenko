962eeb1a641617425b3934534f9583ad1869caef
Sergii <khromchenko13@gmail.com>
2017-10-20 16:02:43 -0700

    Homepage update/cleanup

--- myScript.js
+++ myScript.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
 
     var firmacount = 0;
     $(document).ready(function(){
-        
-        
+          
         $(function(){
           // get hash value
           var hash = window.location.hash;
@@ -27,25 +26,11 @@ $(document).ready(function(){
         $('.toggle').toggles({
           drag: true, // allow dragging the toggle between positions
           click: true, // allow clicking on the toggle
-          text: {
-            on: 'English', // text for the ON position Inglés
-            off: 'Español' // and off
-          },
           on: true, // is the toggle ON on init
           animate: 250, // animation time (ms)
           width: 100, // width used if not set in css
           height: 30, // height if not set in css
         });
-
-
-        ingles();
-
-        $('.toggle').on('toggle', function(e, active) {
-          if (active) {
-            ingles();
-          } else {
-            espanol();
-          }});
 
     });
 
@@ -60,13 +45,13 @@ $(document).ready(function(){
 
     $( ".body" ).scroll(function() {
         if ($('.line').visible(true,true)) {
-            $(".background").css("background", "linear-gradient(#2ecc71, #16a085)");
+            $(".background").css("background", "linear-gradient(#000000, #000000)");
         }
 
         if ($('.show1').visible(false,true)) {
             $('.show1').addClass("animated slideInLeft visible");
             $('.show1').removeClass("hidden");
-            $(".background").css("background", "linear-gradient(#ecf0f1, #7f8c8d)");
+            $(".background").css("background", "linear-gradient(#000000, #000000)");
         }
 
         if ($('.show2').visible(true,true)) {
@@ -75,17 +60,17 @@ $(document).ready(function(){
 
         if ($('.show3').visible(true,true)) {
             $('.show3').addClass("animated slideInLeft visible");
-            $(".background").css("background", "linear-gradient(#1abc9c, #16a085)");
+            $(".background").css("background", "linear-gradient(#000000, #000000)");
         }
 
         if ($('.show4').visible(true,true)) {
             $('.show4').addClass("animated slideInRight visible");
-            $(".background").css("background", "linear-gradient(#9b59b6, #34495e)");
+            $(".background").css("background", "linear-gradient(#000000, #000000)");
         }
 
         if ($('.show5').visible(true,true)) {
             $('.show5').addClass("animated slideInRight visible");
-            $(".background").css("background", "linear-gradient(#7f8c8d,#e74c3c)");
+            $(".background").css("background", "linear-gradient(#000000, #000000)");
         }
 
         if($(".firma").visible(true)&& firmacount == 0){
@@ -104,18 +89,5 @@ $(document).ready(function(){
             ]);
         }
     });
-    
-
-
-        
-    function ingles(){
-        $(".espanol").hide();
-        $(".ingles").show();
-    }
-
-    function espanol(){
-        $(".ingles").hide();
-        $(".espanol").show();
-    }
 
 })
